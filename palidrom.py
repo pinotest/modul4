407098c1e6b5420a757e19dd351d7101954487ea
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2020-05-28 19:57:39 +0200

    Add second version solution

--- palidrom.py
+++ palidrom.py
@@ -1,2 +1,14 @@
+#first version of solution
 def check_palidrom(text):
+    for i in range(len(text)//2):
+        if text[i].lower() != text[-i-1].lower():
+            return False
     return True
+
+#second version
+def check_palidrom_sec(text):
+    return any(False if text[i].lower() != text[-i-1].lower() else True for i in range(len(text)//2))
+
+print(check_palidrom_sec("Kajak"))
+print(check_palidrom_sec("112"))
+print(check_palidrom_sec("1"))
