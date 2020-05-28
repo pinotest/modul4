5ad0f5de9b65bbae5b5a9cb0c6ef897ac59fb1b8
Piotr Nowak <piotr.cezary.nowak@gmail.com>
2020-05-28 21:05:59 +0200

    Add function documentation

--- palidrom.py
+++ palidrom.py
@@ -1,14 +1,22 @@
 #first version of solution
-def check_palidrom(text):
+def check_palidrom_first(text):
     for i in range(len(text)//2):
         if text[i].lower() != text[-i-1].lower():
             return False
     return True
 
 #second version
-def check_palidrom_sec(text):
+def check_palidrom(text):
+    '''
+    check_palidrom(string)
+
+    Returns True if given string is a palindrome - string that you can read forehead or reverse with the same meaning eg. level, 101.  
+    '''
     return any(False if text[i].lower() != text[-i-1].lower() else True for i in range(len(text)//2))
 
-print(check_palidrom_sec("Kajak"))
-print(check_palidrom_sec("112"))
-print(check_palidrom_sec("1"))
+print((check_palidrom_first("Kajak")))
+
+print((check_palidrom("Kajak")))
+
+# print(check_palidrom_sec("112"))
+# print(check_palidrom_sec("1"))
